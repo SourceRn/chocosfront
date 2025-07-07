8f7fe6a3107f50d7190e2b3f03b87ddd96052ee7
SourceRn <ramnievesg@gmail.com>
2025-07-07 17:18:25 -0600

    ajustes en footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -5,11 +5,6 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <p>&copy; 2025 Chocos El Inge. Todos los derechos reservados.</p>
-        <ul className="social-links">
-          <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-        </ul>
       </div>
     </footer>
   );
